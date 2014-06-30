72b460d06d60d052cee502f4fbf9d08169b2c70b
marylynx <tojibaeva.maria@gmail.com>
2014-06-30 14:03:07 -0400

    gotchas

---
+++ nodejs/chapter4/server.js
@@ -0,0 +1,20 @@
+﻿/*for(var i = 0; i < 5; i++) {
+  (function() {
+    setTimeout( function() { console.log(i); }, 500*i);
+  })();
+}*/
+
+/*for(var i = 0; i < 5; i++) {
+ setTimeout(function() {
+  console.log(i);
+ }, 100);
+ i++; // 6 6 6 
+} */
+
+for(var i = 0; i < 5; i++) {
+var j = i;
+ setTimeout(function() {
+  console.log(j);
+ }, 100); // 4 4 4
+ i++;
+}
